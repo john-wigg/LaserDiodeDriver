5c03d2932bf99daa76c9ba843e3f7a1df400937b
John Wigg <john.wigg@uni-jena.de>
2020-12-07 16:17:56 +0100

    add license notice to Setup.ino

--- arduino_sketches/Setup/Setup.ino
+++ arduino_sketches/Setup/Setup.ino
@@ -1,7 +1,24 @@
-/* Sketch based on MCP4728 program address by Jan Knipper.
- * https://github.com/jknipper/mcp4728_program_address
+/* Copyright (C) 2020 by Jan Knipper, John Wigg
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see [http://www.gnu.org/licenses/].
+ *
+ * ----------------------------------------------------------------------
+ *
+ * This program is based on MCP4728 program address by Jan Knipper:
+ * [https://github.com/jknipper/mcp4728_program_address]
  * 
- * Used with permission from the author.
+ * It is used and licensed with permission from the author.
  */
 
 #include <Arduino.h>
