71749b6d6be9572e74d68e5b4cf21ffa16852f6b
dschr <laserengine@gmx.com>
2021-09-11 19:46:56 +0200

    correct typo & delete printouts

--- Arduino.h
+++ Arduino.h
@@ -32,7 +32,7 @@
 #include "serial/serial.h"
 
 #define BAUD 115200
-#defube FLOWCTRL "flowcontrol_software"
+#define FLOWCTRL "flowcontrol_software"
 
 // Codes for communication via Serial
 #define CODE_OPEN 0x00
